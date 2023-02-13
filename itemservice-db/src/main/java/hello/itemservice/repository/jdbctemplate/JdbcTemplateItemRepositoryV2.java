f9d63007965f02d8be792e50d0706ba157f0d2c6
한영수 <besthys7@gmail.com>
2023-02-13 17:41:19 +0900

    JdbcTemplate - 이름 지정 파라미터 2

--- itemservice-db/src/main/java/hello/itemservice/repository/jdbctemplate/JdbcTemplateItemRepositoryV2.java
+++ itemservice-db/src/main/java/hello/itemservice/repository/jdbctemplate/JdbcTemplateItemRepositoryV2.java
@@ -64,7 +64,7 @@ public class JdbcTemplateItemRepositoryV2 implements ItemRepository {
 
     @Override
     public Optional<Item> findById(Long id) {
-        String sql = "select id, item_name, price, quantity from item where id= :id";
+        String sql = "select id, item_name as itemName, price, quantity from item where id= :id";
 
         try {
             Map<String, Object> param = Map.of("id", id);
