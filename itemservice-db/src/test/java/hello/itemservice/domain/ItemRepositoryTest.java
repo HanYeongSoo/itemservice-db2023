9b803d0b6da62e0ce4fdde50a855394ec224462c
한영수 <besthys7@gmail.com>
2023-02-15 16:38:48 +0900

    테스트 - @Transactional

--- itemservice-db/src/test/java/hello/itemservice/domain/ItemRepositoryTest.java
+++ itemservice-db/src/test/java/hello/itemservice/domain/ItemRepositoryTest.java
@@ -9,21 +9,23 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-
+@Transactional
 @SpringBootTest
 class ItemRepositoryTest {
-
     @Autowired
     ItemRepository itemRepository;
 
     //트랜잭션 관련 코드
+    /*
     @Autowired
     PlatformTransactionManager transactionManager;
     TransactionStatus status;
@@ -33,6 +35,7 @@ class ItemRepositoryTest {
         //트랜잭션 시작
         status = transactionManager.getTransaction(new DefaultTransactionDefinition());
     }
+     */
 
     @AfterEach
     void afterEach() {
@@ -41,9 +44,10 @@ class ItemRepositoryTest {
             ((MemoryItemRepository) itemRepository).clearStore();
         }
         //트랜잭션 롤백
-        transactionManager.rollback(status);
+//        transactionManager.rollback(status);
     }
 
+    @Commit
     @Test
     void save() {
         //given
